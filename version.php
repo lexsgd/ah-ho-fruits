481f25e674507eaa23124d048f8d2c78c26fa074
Lex <lex@peacom.co>
2026-01-25 14:12:01 +0800

    test: add version.php to diagnose PHP version issue

---
+++ version.php
@@ -0,0 +1,7 @@
+<?php
+header('Content-Type: text/plain');
+echo "PHP Version: " . phpversion() . "\n";
+echo "Server: " . $_SERVER['SERVER_SOFTWARE'] . "\n";
+echo "Document Root: " . $_SERVER['DOCUMENT_ROOT'] . "\n";
+echo "Script Filename: " . $_SERVER['SCRIPT_FILENAME'] . "\n";
+echo "\nAll working!\n";
